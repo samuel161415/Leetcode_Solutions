ababb40d41021221fe15a70d46ce2ca207000dc1
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-04-19 12:22:07 +0300

    Time: 3 ms (80.62%), Space: 87.9 MB (57.27%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.java
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.java
@@ -1,16 +1,16 @@
 class Solution {
     public long getDescentPeriods(int[] prices) {
-        long r = prices.length;
-        int m = 1;
+        long result = prices.length;
+        int temp = 1;
         for (int i = 1; i < prices.length; i++) {
             if (prices[i-1] - prices[i] == 1) {
-                r += (1*m);
-                m++;
+                result += (1*temp);
+                temp++;
             } else {
-                m = 1;
+                temp = 1;
             }
         }
         
-        return r;
+        return result;
     }
 }
