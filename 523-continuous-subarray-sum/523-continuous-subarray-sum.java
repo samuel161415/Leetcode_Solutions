d9efa5ce1be8b4b367346a94af6e21d0624a1ac1
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-05-05 23:51:16 +0300

    Time: 28 ms (56.44%), Space: 100.1 MB (5.26%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.java
@@ -2,19 +2,16 @@ class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
          if(nums.length==1) return false;
         
-        int[] psum=new int[nums.length];
+        int psum = nums[0];
         
-        psum[0]=nums[0];
-        for(int i=1;i<nums.length;i++){
-        psum[i]=psum[i-1]+nums[i];
-        }
         HashMap<Integer,Integer> hash=new HashMap<>();
-         hash.put(psum[0]%k,0);
-        for(int i=1;i<psum.length;i++){
-           
-         if((psum[i]%k==0)||(hash.containsKey(psum[i]%k)&&(i-hash.get(psum[i]%k)>1))) return true;
-         if(!hash.containsKey(psum[i]%k))
-            hash.put(psum[i]%k,i);
+        hash.put(psum%k,0);
+        
+        for(int i=1;i<nums.length;i++){ 
+            psum += nums[i];
+            if((psum%k==0)||(hash.containsKey(psum%k)&&(i-hash.get(psum%k)>1))) return true;
+            if(!hash.containsKey(psum%k))
+               hash.put(psum%k,i);
         }
         
         return false;
