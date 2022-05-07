e4e831e3b82b12fd5af8a04028163496ab976789
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-05-07 12:02:41 +0300

    Time: 17 ms (5.82%), Space: 47.3 MB (5.10%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-       Arrays.sort(trips,Comparator.comparing(trip->trip[1])); // sort according to starting position
-        PriorityQueue<int[]> pq=new PriorityQueue<>(Comparator.comparing(trip->trip[2])); // store according to 
+       Arrays.sort(trips,Comparator.comparing(trip->trip[1])); 
+        PriorityQueue<int[]> pq=new PriorityQueue<>(Comparator.comparing(trip->trip[2])); 
         for(int[] trip:trips){
-            while(!pq.isEmpty()&&trip[1]>=pq.peek()[2]){  // if next trip's starting pos> previous end some passangers need to  
-              capacity+=pq.poll()[0];                    // drop off from car and capacity increases
+            while(!pq.isEmpty()&&trip[1]>=pq.peek()[2]){  
+              capacity+=pq.poll()[0];                    
             }
-              capacity-=trip[0];     // adding passangers to car will decrease cars capacity
+              capacity-=trip[0];    
             if(capacity<0) return false;
-            pq.add(trip);       // puting the current element into priority queue
+            pq.add(trip);       
           }
         return true;
     }
