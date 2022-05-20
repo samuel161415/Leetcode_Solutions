e8aaad34fa1b9fafc4d5f56cb34b0a3a857ecaad
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-05-20 22:11:44 +0300

    Time: 0 ms (100.00%), Space: 41.5 MB (14.83%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+      int[][] dp=new int[m][n];
+      return totalPath(0,0,dp,m,n);
+    }
+    public int totalPath(int i, int j, int[][] dp, int m, int n){
+          if(i==m||j==n) return 0;
+          if(i==m-1&&j==n-1) return 1;
+          
+          if(dp[i][j]==0) 
+              return dp[i][j]=totalPath(i+1,j,dp,m,n)+ totalPath(i,j+1,dp,m,n);
+    
+          else return dp[i][j];
+      }
+}
