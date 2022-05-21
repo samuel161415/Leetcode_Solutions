b0d53363eea3cc55129a693bb5718414beae5a4a
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-05-21 16:58:54 +0300

    Time: 0 ms (100.00%), Space: 42.1 MB (5.31%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -4,19 +4,12 @@ class Solution {
         return Stairs(n,dp);
     }
     public int Stairs(int n,int[] dp){
-      if(n==0){
-          dp[0]=0; 
-        //  return dp[n];
-      }
-      if(n==1) {
-      dp[1]=1;
-      //return dp[1];
-      }
-      if(n==2) {
-      dp[2]=2;
-      //return dp[2];
-      }
-        if(dp[n]==0) dp[n]=Stairs(n-1,dp)+Stairs(n-2,dp);
+      if(n==0) dp[0]=0; 
+       
+      if(n==1) dp[1]=1;
+      
+      if(n==2) dp[2]=2;
+      if(dp[n]==0) dp[n]=Stairs(n-1,dp)+Stairs(n-2,dp);
         return dp[n];
     }
 }
