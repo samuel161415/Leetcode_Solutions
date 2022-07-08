f3c06e75f1dd236d087ac005f8a487f0743f1b8b
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-07-08 10:26:49 +0300

    Create Domino-Piling.java

---
+++ Domino-Piling.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class codeforce {
+   public static void main(String[] args) {
+     Scanner input=new Scanner(System.in);
+        int m=input.nextInt();
+        int n=input.nextInt();
+       
+        System.out.println(m*n/2);
+   }
+}
