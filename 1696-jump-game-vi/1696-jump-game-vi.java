dfec311ec707548292f7575a2d57133b68946941
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-07-10 02:06:41 +0300

    Time: 23 ms (96.61%), Space: 57.6 MB (92.42%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -1,24 +1,13 @@
 class Solution {
-  public int maxResult(int[] nums, int k) {
-        Deque<Pair<Integer, Integer>> deque = new LinkedList<>() {{
-            offer(new Pair<>(0, nums[0]));
-        }};
-        int max = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            while (!deque.isEmpty() && deque.peekFirst().getKey() < i - k) {
-                deque.pollFirst();
-            }
-
-            max = nums[i] + (deque.isEmpty() ? 0 : deque.peekFirst().getValue());
-
-            while (!deque.isEmpty() && deque.peekLast().getValue() <= max) {
-                deque.pollLast();
-            }
-
-            deque.offerLast(new Pair<>(i, max));
-        }
-
-        return max;
+ public int maxResult(int[] nums, int k) {
+    Deque<Integer> deque = new ArrayDeque<>();
+    deque.add(0);
+    for(int i = 1; i < nums.length; i++) {
+        nums[i] += nums[deque.peek()];
+        if(i - k == deque.peek()) deque.poll();
+        while(!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) deque.pollLast();
+        deque.add(i);
     }
+    return nums[nums.length - 1];
+}
 }
