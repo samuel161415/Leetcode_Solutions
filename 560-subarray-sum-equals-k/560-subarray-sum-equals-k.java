3f27acf2807b41f3f9f9271c291111fa5020495c
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-07-11 09:46:50 +0300

    Time: 19 ms (99.77%), Space: 45.7 MB (94.64%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,20 +1,17 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int[] psum=new int[nums.length];
-        psum[0]=nums[0];
-        for(int i=1;i<nums.length;i++){
-            psum[i]=psum[i-1]+nums[i];
-}
+       int[] psum=new int[nums.length+1];
+        for(int i=1;i<psum.length;i++){
+            psum[i]=psum[i-1]+nums[i-1];
+        }
         HashMap<Integer,Integer> map=new HashMap<>();
         int count=0;
-        for(int i=0;i<psum.length;i++){
-          if(psum[i]==k){
-              if(map.containsKey(psum[i]-k)) count+=map.get(psum[i]-k);
-               count++;
-          }
-            else if(map.containsKey(psum[i]-k)) count+=map.get(psum[i]-k);
-            map.put(psum[i], map.getOrDefault(psum[i], 0) + 1);
+        for(int i=1;i<psum.length;i++){
+            if(psum[i]==k) count++;
+             if(map.containsKey(psum[i]-k)) count+=map.get(psum[i]-k);
+            map.put(psum[i],map.getOrDefault(psum[i],0)+1);
         }
         return count;
+        
     }
 }
