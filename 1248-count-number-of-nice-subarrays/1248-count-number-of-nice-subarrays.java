f249c0708c25c04fa23c74504f9ffad3eba3f9bc
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-07-11 11:29:11 +0300

    Time: 52 ms (31.82%), Space: 51.7 MB (85.95%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,29 +1,24 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        
-      
-     int s=0,e=nums.length;
-   
-    for(int i=0;i<e;i++){
-        if(nums[i]%2!=0){
-            nums[i]=1;
-        }else{
-            nums[i]=0;
+        for(int i=0;i<nums.length;i++){  // converting problems to prefix sum like 560. Subarray Sum Equals K questions
+            if(nums[i]%2==1) nums[i]=1;
+            else nums[i]=0;
         }
-    }
+        int[] psum=new int[nums.length];
+        psum[0]=nums[0];
+        for(int i=1;i<psum.length;i++){
+            psum[i]=psum[i-1]+nums[i];
+        }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int count=0;
+        for(int i=0;i<psum.length;i++){
+          if(psum[i]==k) count++;
+          if(map.containsKey(psum[i]-k)) count+=map.get(psum[i]-k); 
+          map.put(psum[i],map.getOrDefault(psum[i],0)+1);
+        }
+        
+        return count;
       
-       HashMap<Integer,Integer> p = new HashMap<>();
-        
-     p.put(0,1);   
-     int ans=0,sum=0;
-        
-     for(int i=0;i<nums.length;i++){
-         sum+=nums[i];
-        if(p.containsKey(sum-k)){
-            ans+=p.get(sum-k);
-        } 
-         p.put(sum,p.getOrDefault(sum,0)+1);
-     }   
-       return ans; 
+     
     }
 }
