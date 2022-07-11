f8efbcc9b0882790c6bf0417f5bbb68ab6e676f6
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-07-11 21:35:25 +0300

    Time: 22 ms (37.30%), Space: 50.5 MB (48.51%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,25 +1,22 @@
 import java.util.Map.Entry;
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-      HashMap<Integer,Integer> hash=new HashMap<>();
-        int[] array=new int[k];
+   PriorityQueue<Map.Entry<Integer,Integer>> pq=new PriorityQueue<>((a,b)->{return (b.getValue()-a.getValue());});
+        Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            hash.put(nums[i],hash.getOrDefault(nums[i],0)+1);   
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        int j=0;
-       while(k>0){
-           int idx=0;
-        int maxFreq=0;                       // basically it does is first store values with their corrosponding frequencies
-           for(int i:hash.keySet()){         // then according to that frequency d
-              if(hash.get(i)>maxFreq) {      // thake the most frequent element and set to array and remove from hash
-                  maxFreq=hash.get(i);
-                  idx=i;
-              }    
-           }
-               array[j++]=idx;
-               hash.remove(idx);
-               k--;
-       }
-    return array;
+        
+      for(Map.Entry<Integer,Integer> e:map.entrySet()){
+          pq.add(e);
+      }
+        System.out.println(pq);
+        int[] res=new int[k];
+        for(int i=0;i<k;i++){
+            res[i]=pq.remove().getKey();
+        }
+        
+        
+        return res;
     }
 }
