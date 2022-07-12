a414365834965016a28e68c725568ca49d2192fc
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-07-12 10:49:42 +0300

    Time: 29 ms (88.40%), Space: 118.4 MB (28.06%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+      
+        Arrays.sort(points,(a,b)->(a[0]*a[0]+a[1]*a[1])-(b[0]*b[0]+b[1]*b[1]));
+        
+        return Arrays.copyOf(points,k);
+        
+        
+    }
+}
