0cbea762c2e4672a701610e8cfd4063928bb9fad
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-07-20 16:40:38 +0300

    Time: 7 ms (36.03%), Space: 44 MB (66.65%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,23 +1,19 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-        int[] result=new int[nums1.length];
-        Stack<Integer> stack =new Stack<>();     // I used decreasing monotonic stack                                         
-        HashMap<Integer,Integer> hash=new HashMap<>();  // and hashmap to store element with next greater element
-           for(int i=0;i<nums2.length;i++){              // i.e the removing element is next element to the removed one
-            while(!stack.isEmpty()&& nums2[i]>stack.peek())
-            {
-            hash.put(stack.pop(),nums2[i]);
+       HashMap<Integer,Integer> map=new HashMap<>();
+        Stack<Integer> stack=new Stack<>();
+        for(int i=0;i<nums2.length;i++){
+            while(!stack.isEmpty()&&stack.peek()<nums2[i]){
+             map.put(stack.pop(),nums2[i]);
             }
              stack.push(nums2[i]);
-           }
-           
-        for(int j=0;j<nums1.length;j++){
-            if(hash.containsKey(nums1[j]))
-                result[j]=hash.get(nums1[j]);
-            else result[j]=-1;
         }
-        return result;
-       
-    }
-}
+        int[] res=new int[nums1.length];
+        Arrays.fill(res,-1);
+        for(int i=0;i<nums1.length;i++){
+            if(map.containsKey(nums1[i])) res[i]=map.get(nums1[i]);
+        }
+        return res;
+        
+    }}
