4953e9a933d58ff823b582c17f57961df23bf77d
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-08-16 23:13:48 -0700

    Time: 2 ms (21.85%), Space: 41.8 MB (76.07%) - LeetHub

---
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        int maxDepth=getWidth(root)-1;
+        System.out.println(maxDepth);
+        
+        return helper(root,0,maxDepth);
+        
+    }
+    public int getWidth(TreeNode root){
+        if(root==null) return 0;
+        int left=1+getWidth(root.left);
+        int right=1+getWidth(root.right);
+        return Math.max(left,right);
+    }
+    public TreeNode helper(TreeNode root,int depth,int maxDepth){
+        
+      if(root==null) return null ;  
+      if(depth==maxDepth) return root;
+        
+       TreeNode left=helper(root.left,depth+1,maxDepth);
+       TreeNode right=helper(root.right,depth+1,maxDepth);
+        
+        if(left!=null&&right!=null) return root;
+        else if (left!=null) return left;
+        else return right;
+         
+        
+    }
+}
