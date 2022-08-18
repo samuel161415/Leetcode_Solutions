2f201c481cc242bd54764f2394757edb33160c91
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-08-18 00:27:29 -0700

    Time: 34 ms (79.97%), Space: 43.7 MB (18.25%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.java
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    Node[] visited=new Node[101];
+    public Node cloneGraph(Node node) {
+        Arrays.fill(visited,null);
+        
+        if(node==null) return null;
+       return helper(node,new Node());
+        
+    }
+    public Node helper(Node node, Node newNode){
+        
+        newNode.val=node.val;
+        visited[newNode.val]=newNode;
+        for(Node nNode:node.neighbors){
+            
+            if(visited[nNode.val]==null){
+                newNode.neighbors.add(helper(nNode,new Node()));
+            }
+            else{
+                newNode.neighbors.add(visited[nNode.val]);
+                continue;
+            }
+        }
+        return newNode;
+        
+    }
+}
