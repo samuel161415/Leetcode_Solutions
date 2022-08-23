37c51e45c3ecbe01479f8df5091c73864fded21d
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-08-23 00:33:54 -0700

    Time: 0 ms (100.00%), Space: 42.3 MB (51.87%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -14,15 +14,16 @@
  * }
  */
 class Solution {
-    List<Integer> list=new ArrayList<>();
+  
     public List<Integer> postorderTraversal(TreeNode root) {
-        dfs(root);
+        List<Integer> list=new ArrayList<>();
+        dfs(root,list);
         return list;
     }
-    public void dfs(TreeNode root){
+    public void dfs(TreeNode root,List<Integer> list){
         if(root==null) return ;
-        dfs(root.left);
-        dfs(root.right);
+        dfs(root.left,list);
+        dfs(root.right,list);
         list.add(root.val);
     }
 }
