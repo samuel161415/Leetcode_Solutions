0cae14f354aac5060a12fa20a48db727aa2c4a4f
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-08-25 03:04:12 -0700

    Time: 691 ms (36.42%), Space: 59.3 MB (87.50%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+      PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
+      boolean[] visited=new boolean[points.length];
+        
+        for(int i=1;i<points.length;i++){
+            int d=Math.abs(points[0][0]-points[i][0])+Math.abs(points[0][1]-points[i][1]);
+            pq.add(new int[]{i,d});
+        }
+        visited[0]=true;
+        
+        int res=0;
+        while(!pq.isEmpty()){
+            int[] curPoint=pq.poll();
+            int idx=curPoint[0];
+            int cdis=curPoint[1];
+            if(!visited[idx]) {
+                visited[idx]=true;
+              res+=cdis;
+            
+            for(int i=0;i<points.length;i++){
+                if(!visited[i]){
+                    int d=Math.abs(points[idx][0]-points[i][0])+Math.abs(points[idx][1]-points[i][1]);
+                    pq.add(new int[]{i,d});
+                }
+            }
+            }
+        }
+        return res;
+    }
+}
