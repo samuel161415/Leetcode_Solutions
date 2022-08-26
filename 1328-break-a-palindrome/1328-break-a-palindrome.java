0031cab87e2d166ab2ca93882e9c00e6749b038d
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-08-26 00:29:22 -0700

    Time: 0 ms (100.00%), Space: 40.2 MB (96.20%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        
+        int[] chara=new int[26];
+        for(int i=0;i<=25;i++){
+            chara[i]=97+i;
+        }
+        if(palindrome.length()==1) return "";
+        StringBuilder sb=new StringBuilder(palindrome);
+        
+        for(int i=0;i<palindrome.length();i++){
+            if(!check(sb.toString())) break;
+            int c=palindrome.charAt(i);
+            if(c>97) {
+                for(int j=0;j<c-97;j++){
+                    char temp=(char)chara[j];
+                    sb.setCharAt(i, temp);
+                    boolean res=check(sb.toString());
+                    if(!res) break;
+                    else sb.setCharAt(i, palindrome.charAt(i));
+                    
+                }
+                 }
+            if(i==palindrome.length()-1&&check(sb.toString())){
+                 sb.setCharAt(i, (char)(palindrome.charAt(i)+1));
+            }
+            
+            
+            
+        }
+        return sb.toString();
+      
+    }
+      public boolean check(String str){
+            int s=0;
+            int e=str.length()-1;
+            while(s<e){
+                if(str.charAt(e)!=str.charAt(s)) return false;
+                s++;
+                e--;
+            }
+            return true;
+        }
+}
