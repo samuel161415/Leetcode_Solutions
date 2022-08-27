33854fb13410be84c170044f453304e8eda50ceb
Samuel Negalign <81478880+samuel161415@users.noreply.github.com>
2022-08-27 03:49:50 -0700

    Create download_More_Ram.java

---
+++ download_More_Ram.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class moreRam {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int t=input.nextInt();
+        for(int i=0;i<t;i++){
+          int n=input.nextInt();
+          int k=input.nextInt();
+          int[] ramSoft=new int[n];
+          int[] addRam=new int[n];
+           for(int j=0;j<n;j++){
+             ramSoft[j]=input.nextInt();
+           }
+           for(int j=0;j<n;j++){
+            addRam[j]=input.nextInt();
+          }
+          int[][] arr=new int[n][2];
+          for(int j=0;j<n;j++){
+            arr[j][0]=ramSoft[j];
+            arr[j][1]=addRam[j];
+          }
+          Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
+          int totalSum=k;
+          for(int j=0;j<n;j++){
+            if(arr[j][0]<=totalSum) totalSum+=arr[j][1];
+          }
+          System.out.println(totalSum);
+        }
+ 
+    }
+}
+// It is gready and idea is sort by
